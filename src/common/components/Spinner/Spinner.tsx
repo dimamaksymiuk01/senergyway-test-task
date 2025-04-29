d59002be50539fdab944369b7b50cc1494355eb4
dimamaksymiuk01 <dimamaksymiuk01@gmail.com>
2025-04-29 14:35:13 +0300

    add loading spinner

--- src/common/components/Spinner/Spinner.tsx
+++ src/common/components/Spinner/Spinner.tsx
@@ -1,12 +1,12 @@
 import { Loader2 } from 'lucide-react';
-import React from 'react';
+import { FC } from 'react';
 
 interface SpinnerProps {
   size?: number;
   className?: string;
 }
 
-export const Spinner: React.FC<SpinnerProps> = ({ size = 24, className = '' }) => {
+export const Spinner: FC<SpinnerProps> = ({ size = 24, className = '' }) => {
   return (
     <Loader2
       className={`animate-spin text-gray-500 ${className}`}
