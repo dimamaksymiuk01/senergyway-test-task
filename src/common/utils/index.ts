924249870eb55a1004d531d92a951a66a3d536eb
dimamaksymiuk01 <dimamaksymiuk01@gmail.com>
2025-04-29 15:57:52 +0300

    refactor

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -2,4 +2,3 @@ export * from './updatePageUrl.ts';
 export * from './buildLayout.ts';
 export * from './getVisibleCompanies.ts';
 export * from './updateLayoutAfterClose.ts';
-export * from './getCompanyInfoFields.ts';
